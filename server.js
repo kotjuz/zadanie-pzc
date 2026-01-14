41a3060ec26c0b46e86617987f41a99f31921e6f
Rafal Kotarski <105173248+kotjuz@users.noreply.github.com>
2026-01-14 13:01:38 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,29 @@
+const http = require('http');
+
+const port = process.env.PORT || 3000;
+
+const requestHandler = (req, res) => {
+    const now = new Date();
+    const timeString = now.toISOString().split('.')[0] + '+00:00';
+    
+    const htmlContent = `<html>
+<head>
+<title>Rozwiązanie zadania PZC 2.1</title>
+</head>
+<body>
+${timeString}
+</body>
+</html>`;
+
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+    res.end(htmlContent);
+};
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, (err) => {
+    if (err) {
+        return console.log('Błąd:', err);
+    }
+    console.log(`Serwer działa na porcie ${port}`);
+});
